1af5581c73fc531dce1a7deaa56787069d5bb354
Joseph Surin <joseph.surin@gmail.com>
2019-10-08 11:25:09 +1100

    start about page styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,28 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Banner from '../components/banner'
 
-import '../styles/banner.sass'
+import '../styles/about.sass'
 
 const AboutPage = () => {
     return (
         <Layout>
             <SEO title="About" />
             <Banner class_suffix="about"/>
+            <div className="about-container">
+                <div className="about-content">
+                    <h1>hhhh</h1>
+                    <h1>hhhh</h1>
+                    <h1>hhhh</h1>
+                    <h1>hhhh</h1>
+                    <h1>hhhh</h1>
+                    <h1>hhhh</h1>
+                    <h1>hhhh</h1>
+                    <h1>hhhh</h1>
+                    <h1>hhhh</h1>
+                    <h1>hhhh</h1>
+                    <h1>hhhh</h1>
+                </div>
+            </div>
         </Layout>
     )
 
