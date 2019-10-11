c66728670d0ba11be900b870c053204d52a8999d
Joseph Surin <joseph.surin@gmail.com>
2019-10-12 01:49:55 +1100

    link import fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Link from 'gatsby'
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Banner from '../components/banner'
