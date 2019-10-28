37f3c2f54f000adce3e3228388dc56bb5c773b56
Asil Mian <asilmian@gmail.com>
2019-10-28 23:18:02 +1100

    added socials to gallery

---
+++ src/gatsby-theme-gallery/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Socials from '../../components/socials'
+
+const FooterGall = () => (
+    <footer>
+        <div>
+            <Socials/>
+        </div>
+    </footer>
+)
+
+export default FooterGall
