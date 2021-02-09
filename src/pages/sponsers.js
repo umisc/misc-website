9eca7d6b23b976aea8e1c302b7bc482cf5ca7ed4
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2021-02-09 19:58:02 +0530

    created the sponsers page with basic designs

---
+++ src/pages/sponsers.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import atlassian_logo from '../images/atlassian-logo.png'
+import cybersecpeople_logo from '../images/cybersecpeople-logo.png'
+//import dronesec_logo from '../images/dronesec-logo.png'
+//import kaspersky_logo from '../images/kaspersky-logo.png'
+//import pentesterlab_logo from '../images/pentesterlab-logo.png'
+//import commbank_logo from '../images/commbank-logo.png'
+
+
+import '../styles/sponsers.sass'
+
+const SponsersPage = () => {
+    return (
+        <Layout>
+            <SEO title="Sponsers" />
+            <div className="gallery-container">
+                <h1 className="gold-heading">Gold Sponsers</h1>
+                <div className="sponsors-container">
+	                <div className="sponsors-logos">
+	                    <a href="https://www.atlassian.com/"><img src={atlassian_logo} className="atlassian-logo"/></a>
+	                </div>
+	                <div className="sponsors-logos">
+	                    <a href="https://www.cybersecpeople.com/"><img src={cybersecpeople_logo} className="cybersecpeople-logo"/></a>
+	                </div>
+	                {/*<div className="sponsors-logos">
+	                                    <a href="https://dronesec.com/"><img src={dronesec_logo} className="dronesec-logo"/></a>
+	                                </div>
+	                                <div className="sponsors-logos">
+	                                    <a href="https://www.kaspersky.com.au/"><img src={kaspersky_logo} className="kaspersky-logo"/></a>
+	                                </div>
+	                                <div className="sponsors-logos">
+	                                    <a href="https://pentesterlab.com/"><img src={pentesterlab_logo} className="pentesterlab-logo"/></a>
+	                                </div>
+	                                <div className="sponsors-logos">
+	                                    <a href="https://www.commbank.com.au/"><img src={commbank_logo} className="commbank-logo"/></a>
+	                                </div>*/}
+                </div>
+            </div>
+        </Layout>
+    )
+
+} 
+export default SponsersPage
