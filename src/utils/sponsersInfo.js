03ba02e1e2d622804843d16ded567611bc90490d
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2021-02-13 16:43:12 +0530

    Added sponsers carousel on the about page

---
+++ src/utils/sponsersInfo.js
@@ -0,0 +1,33 @@
+
+export const sponsersInfo = [
+  {
+  	name: "Atlassian Logo",
+  	image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  	href: "https://www.atlassian.com/"
+  },
+  {
+  	name: "CyberSecPeople Logo",
+  	image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  	href: "https://www.cybersecpeople.com/"
+  },
+  {
+  	name: "Dronsec Logo",
+  	image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  	href: "https://dronesec.com/"
+  },
+  {
+  	name: "Kaspersky Logo",
+  	image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  	href: "https://www.kaspersky.com.au/"
+  },
+  {
+  	name: "PentesterLab Logo",
+  	image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  	href: "https://pentesterlab.com/"
+  },
+  {
+  	name: "CommBank Logo",
+  	image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  	href: "https://www.commbank.com.au/"
+  },
+];
