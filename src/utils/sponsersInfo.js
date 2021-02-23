cea0ee4153b5c81b9b74882ade0e03e7f335e171
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2021-02-23 16:40:10 +0530

    Added images to the utils page

--- src/utils/sponsersInfo.js
+++ src/utils/sponsersInfo.js
@@ -1,27 +1,33 @@
+import atlassian_logo from '../images/atlassian-logo.png'
+import cybersecpeople_logo from '../images/cybersecpeople-logo.png'
+import dronsec_logo from '../images/dronesec-logo.jpg'
+import kaspersky_logo from '../images/kaspersky-logo.jpg'
+import pentesterlab_logo from '../images/pentesterlab-logo.jpg'
+
 export const sponsersInfo = [
   {
   	name: "Atlassian Logo",
-  	image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  	image: atlassian_logo,
   	href: "https://www.atlassian.com/"
   },
   {
   	name: "CyberSecPeople Logo",
-  	image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  	image: cybersecpeople_logo,
   	href: "https://www.cybersecpeople.com/"
   },
   {
   	name: "Dronsec Logo",
-  	image: "https://ibb.co/3rwq1pj",
+  	image: dronsec_logo,
   	href: "https://dronesec.com/"
   },
   {
   	name: "Kaspersky Logo",
-  	image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  	image: kaspersky_logo,
   	href: "https://www.kaspersky.com.au/"
   },
   {
   	name: "PentesterLab Logo",
-  	image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  	image: pentesterlab_logo,
   	href: "https://pentesterlab.com/"
   },
   {
