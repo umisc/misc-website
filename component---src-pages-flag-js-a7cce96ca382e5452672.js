d82076ed91e630ea4ad56d32af81eaac155006a8
verbal-noun <kahsan@student.unimelb.edu.au>
2021-03-20 20:23:34 +1100

    Updates

---
+++ component---src-pages-flag-js-a7cce96ca382e5452672.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{P23G:function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),l=a.n(t);n.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement("center",null,l.a.createElement("div",{style:{width:"600px",marginTop:"300px"}},"ok fine you can have a flag... ",l.a.createElement("code",null,"MISCWEBCTF{it_wont_always_be_this_easy}"))))}}}]);
+//# sourceMappingURL=component---src-pages-flag-js-a7cce96ca382e5452672.js.map
